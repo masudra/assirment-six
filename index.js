7e76b0ecc9b80aedfadd674f4613beb24ca0ee3e
masudra <masudrana6278312@gmail.com>
2023-03-03 00:35:11 +0600

    link function add on js

---
+++ index.js
@@ -0,0 +1,10 @@
+const aiLinkFun= async () =>{
+    const url =`https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data.tools);
+}
+
+
+
+aiLinkFun();
