9bdbdf5a11dd7f0f236a26557a91e89b38a8ed53
masudra <masudrana6278312@gmail.com>
2023-03-03 01:00:23 +0600

    all card add

--- index.js
+++ index.js
@@ -6,5 +6,37 @@ const aiLinkFun= async () =>{
 }
 
 
+const displayData = cards =>{
+    const allCards = document.getElementById('card-make');
+    
+    cards.forEach(card =>{
+        const cardCreate = document.createElement('div')
+        cardCreate.classList.add('col')
+        cardCreate.innerHTML =`
+        <div class="card">
+                        <img src="${card.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title fw-bold">Features</h5>
+                        <h5 class="card-title">${card.features}</h5>
+                          <hr>
+                         <div class="d-flex justify-content-between">
+                        <div> 
+                        <h5 class="card-title fw-bold">${card.name}</h5>
+                        <p> <i class="fa-solid fa-calendar-days"></i> ${card.published_in}</p>
+                        </div>
+                        <div class="my-auto">
+                        <button class="border border-0 bg-white text-danger">  <i class="fs-1 fa-regular fa-circle-right"></i></i></button></div>
+                         </div>
+                        </div>
+                      </div>
+
+        `;
+        allCards.appendChild(cardCreate);
+        
+        
+    });
+    
+}
+
 
 aiLinkFun();
