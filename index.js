204137d657a51bfbbb33e24b73f2a40b0ba01c85
masudra <masudrana6278312@gmail.com>
2023-03-03 21:18:04 +0600

    mordal btn add

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ const aiLinkFun= async () =>{
 
 const displayData = cards =>{
     const allCards = document.getElementById('card-make');
+
+    // See More Btn 
+    const showAll = document.getElementById('show-all-btn')
+    if(cards.length >6){
+        cards =cards.slice(0,6);
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
+
+         
     
-    cards.forEach(card =>{
+   cards.forEach(card =>{
         const cardCreate = document.createElement('div')
         cardCreate.classList.add('col')
         cardCreate.innerHTML =`
@@ -17,7 +29,8 @@ const displayData = cards =>{
                         <img src="${card.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                         <h5 class="card-title fw-bold">Features</h5>
-                        <h5 class="card-title">${card.features}</h5>
+                    <h5 class="card-title">${card.features}}
+                   
                           <hr>
                          <div class="d-flex justify-content-between">
                         <div> 
@@ -25,7 +38,9 @@ const displayData = cards =>{
                         <p> <i class="fa-solid fa-calendar-days"></i> ${card.published_in}</p>
                         </div>
                         <div class="my-auto">
-                        <button class="border border-0 bg-white text-danger">  <i class="fs-1 fa-regular fa-circle-right"></i></i></button></div>
+                        <button type="button" class="border border-0 bg-white text-danger" data-bs-toggle="modal" data-bs-target="#cardsModal">
+                        <i class="fs-1 fa-regular fa-circle-right"></i>
+  </button>
                          </div>
                         </div>
                       </div>
@@ -33,10 +48,33 @@ const displayData = cards =>{
         `;
         allCards.appendChild(cardCreate);
         
-        
     });
+    trogelspiner(false);
+
     
 }
+
+
+ //  btn spiner
+ const trogelspiner = isLoding =>{
+    const lode = document.getElementById('loder');
+
+    if(isLoding == true){
+        lode.classList.remove('d-none')
+    }
+    else{
+        lode.classList.add('d-none')
+    }
+}
+// click btn for show all  card 
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    trogelspiner(true);
+    
+   
+});
+
+
+
 
 
 aiLinkFun();
